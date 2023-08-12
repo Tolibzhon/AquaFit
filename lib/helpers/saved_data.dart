a9389ec008f157c6ff8842a6573d52b463b792e2
Tolibzhon <sidikovtolibjon2000@gmail.com>
2023-08-12 16:51:16 +0600

    add

--- lib/helpers/saved_data.dart
+++ lib/helpers/saved_data.dart
@@ -32,7 +32,6 @@ class SavedData {
     prefs.setStringList('IsChek', isChek);
   }
 
-
   static Future<int> getDay() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt('Day') ?? 0;
@@ -41,5 +40,65 @@ class SavedData {
   static Future<void> setDay(int day) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setInt('Day', day);
+  }
+
+  static Future<int> getOne() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('One') ?? 0;
+  }
+
+  static Future<void> setOne(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('One', day);
+  }
+
+  static Future<int> getTwo() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Two') ?? 0;
+  }
+
+  static Future<void> setTwo(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('Two', day);
+  }
+
+  static Future<int> getThree() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Three') ?? 0;
+  }
+
+  static Future<void> setThree(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('Three', day);
+  }
+
+  static Future<int> getFive() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Five') ?? 0;
+  }
+
+  static Future<void> setFive(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('Five', day);
+  }
+
+  static Future<int> getSix() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Six') ?? 0;
+  }
+
+  static Future<void> setSix(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('Five', day);
+  }
+
+  static Future<int> getSeven() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Seven') ?? 0;
+  }
+
+  static Future<void> setSeven(int day) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('Seven', day);
   }
 }
